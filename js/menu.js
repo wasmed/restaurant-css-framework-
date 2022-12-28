15fa4acfab1884c57c9f81fbaf91d4d99e537e3a
wasmed <wasmedbxl@gmail.com>
2022-12-28 17:18:11 +0100

    fix : menu.css and contact.css

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $(".block").click(function(){
-        $(this).toggleClass('active');
-        $(".price").removeClass('active');
-        $(".active .price").addClass('active');
-    });
-    
-    });
